e4cf5e45fea538bee2583b6d4fb4397fa3d6a69d
Luc van Koppen <luc@udoc.nl>
2022-04-04 17:14:30 +0200

    fix infinite loop

--- Main.java
+++ Main.java
@@ -8,61 +8,54 @@ public class Main {
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        String menu = """
-                Menu
-                1) Lijst met examens
-                2) Lijst met studenten
-                3) Nieuwe student inschrijven
-                4) Student verwijderen
-                5) Examen afnemen
-                6) Is student geslaagd voor test?
-                7) Welke examens heeft student gehaald?
-                8) Welke student heeft de meeste examens gehaald?
-                0) Exit
-                Uw keuze:""";
-        int menuInput = 11;
-        while (menuInput > 8 || menuInput < 0) 
-        {
-            try {
-                System.out.println(menu);
-                menuInput = scanner.nextInt();  // Read user input
-            } catch (Exception e) {
-                System.out.println("\nGraag cijfers in voegen!\n");
+        while (true) {
+
+            String menu = """
+                    Menu
+                    1) Lijst met examens
+                    2) Lijst met studenten
+                    3) Nieuwe student inschrijven
+                    4) Student verwijderen
+                    5) Examen afnemen
+                    6) Is student geslaagd voor test?
+                    7) Welke examens heeft student gehaald?
+                    8) Welke student heeft de meeste examens gehaald?
+                    0) Exit
+                    Uw keuze:""";
+
+            int menuInput = 11;
+            System.out.println(menuInput);
+            while (menuInput > 8 || menuInput < 0 || menuInput == 11) {
+                try {
+                    System.out.println(menu);
+                    menuInput = scanner.nextInt();  // Read user input
+                } catch (Exception e) {
+                    System.out.println("\nGraag cijfers in voegen!\n");
+                }
+            }
+            System.out.println("uw keuze is: " + menuInput);  // Output user input
+            if (menuInput == 1) {
+
+            } else if (menuInput == 2) {
+
+            } else if (menuInput == 3) {
+                Gebruiker gebruiker = new Gebruiker("Luc", 21095582, true);
+
+            } else if (menuInput == 4) {
+
+            } else if (menuInput == 5) {
+
+            } else if (menuInput == 6) {
+
+            } else if (menuInput == 7) {
+
+            } else if (menuInput == 8) {
+
+            } else if (menuInput == 0) {
+                System.out.println("Programma sluit af");
+                System.exit(0);
             }
         }
-        System.out.println("uw keuze is: " + menuInput);  // Output user input
-        if (menuInput == 1)
-
-        {
-            
-        } else if (menuInput == 2) 
-        {
-
-        } else if (menuInput == 3) 
-        {
-            Gebruiker gebruiker = new Gebruiker("Luc", 21095582, true);
-
-        } else if (menuInput == 4) 
-        {
-
-        } else if (menuInput == 5) 
-        {
-
-        } else if (menuInput == 6) 
-        {
-
-        } else if (menuInput == 7) 
-        {
-
-        } else if (menuInput == 8) 
-        {
-
-        } else if (menuInput == 0) 
-        {
-            System.out.println("Programma sluit af");
-            System.exit(0);
-        }
-        scanner.close();
     }
 }
 
