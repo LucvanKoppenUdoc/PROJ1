893ffb962e076eb6d1da6a030279cb2f56ff5fc9
Rick van der Knaap <rickknaap@caiway.net>
2022-04-07 16:00:22 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -26,10 +26,26 @@ public class Main {
         keuzeAntwoorden.add("2 poes");
         keuzeAntwoorden.add("3 hondje");
         Vraag vraag = new Vraag("Wat is mijn lievelingsdier?", 1, keuzeAntwoorden, examen);
-        Vraag vraag1 = new Vraag("Wat is mijn lievelingsdier?", 1, keuzeAntwoorden, examen);
-        Vraag vraag2 = new Vraag("Wat is mijn lievelingsdier?", 1, keuzeAntwoorden, examen);
-        Vraag vraag3 = new Vraag("Wat is mijn lievelingsdier?", 1, keuzeAntwoorden, examen);
-        Vraag vraag4 = new Vraag("Wat is mijn lievelingsdier?", 1, keuzeAntwoorden, examen);
+        ArrayList<String> keuzeAntwoorden1 = new ArrayList<String>();
+        keuzeAntwoorden1.add("1 Rikash");
+        keuzeAntwoorden1.add("2 Royalistic");
+        keuzeAntwoorden1.add("3 Gert");
+        Vraag vraag1 = new Vraag("Wat is Roy zijn bijnaam?", 1, keuzeAntwoorden1, examen);
+        ArrayList<String> keuzeAntwoorden2 = new ArrayList<String>();
+        keuzeAntwoorden2.add("1 2");
+        keuzeAntwoorden2.add("2 3");
+        keuzeAntwoorden2.add("3 4");
+        Vraag vraag2 = new Vraag("Wat is 2+2?", 3, keuzeAntwoorden2, examen);
+        ArrayList<String> keuzeAntwoorden3 = new ArrayList<String>();
+        keuzeAntwoorden3.add("1 Delft");
+        keuzeAntwoorden3.add("2 Maassluis");
+        keuzeAntwoorden3.add("3 Arnhem");
+        Vraag vraag3 = new Vraag("Waar woont Daniel?", 2, keuzeAntwoorden3, examen);
+        ArrayList<String> keuzeAntwoorden4 = new ArrayList<String>();
+        keuzeAntwoorden4.add("1 1 meter 50");
+        keuzeAntwoorden4.add("2 1 meter 20");
+        keuzeAntwoorden4.add("3 Ja idd");
+        Vraag vraag4 = new Vraag("Hoelang is een chinees?", 3, keuzeAntwoorden4, examen);
 
         // System.out.println(examen.getVragen());
 
