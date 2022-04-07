ef8b11f1310c85f1f8055ca0936a84037e1f45db
BramS21113653 <101334896+BramS21113653@users.noreply.github.com>
2022-04-07 11:53:57 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -128,6 +128,7 @@ public class Main {
         System.out.println("hallo");
     }
     private static void menuOptieZes(Scanner scanner){
+        //nog afmaken
         scanner.nextLine();
         while (true) {
             try {
