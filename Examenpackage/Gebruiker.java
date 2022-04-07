90647b8852892dddbf7f7a4671e2cb8899a8bb8d
NielsSchool <101334605+NielsSchool@users.noreply.github.com>
2022-04-07 10:18:04 +0200

    Update Gebruiker.java

--- Examenpackage/Gebruiker.java
+++ Examenpackage/Gebruiker.java
@@ -7,6 +7,7 @@ public class Gebruiker {
     private String naam;
     private Integer gebruikerNummer;
     private ArrayList<Resultaat> resulaten = new ArrayList<Resultaat>();
+    public static ArrayList<Gebruiker> gebruikerslijst = new ArrayList<Gebruiker>();
     private boolean admin;
 
 
@@ -15,9 +16,11 @@ public class Gebruiker {
         this.gebruikerNummer = gebruikerNummer;
         this.admin = admin;
 
-        ArrayList<Gebruiker> lijstje = new ArrayList<Gebruiker>();
-        lijstje.add(this);
-        Gebruikerslijst gebruikers = new Gebruikerslijst(lijstje);
+        gebruikerslijst.add(this);
+    }
+
+    public ArrayList<Gebruiker> getGebruikersLijst() {
+        return gebruikerslijst;
     }
 
     public String getNaam() {
@@ -39,14 +42,59 @@ public class Gebruiker {
 //        return resulaten;
 //    }
 
+    public Gebruiker getStudentOnGebruikerNummer(int gebruikernummer) {
+        Gebruiker match = null;
+        for (Gebruiker gebruiker : gebruikerslijst){
+            if (gebruiker.getGebruikerNummer() == gebruikernummer) {
+                match = gebruiker;
+            }
+        }
+        return match;
+    }
+
+    public Gebruiker getStudentOnName(String naam) {
+        Gebruiker match = null;
+        for (Gebruiker gebruiker : gebruikerslijst){
+            if (gebruiker.getNaam() == naam) {
+                match = gebruiker;
+            }
+        }
+        return match;
+    }
+    public boolean verwijderStudent(String naam) {
+        Gebruiker match = null;
+        boolean removed = false;
+        for (Gebruiker gebruiker : gebruikerslijst){
+            if (gebruiker.getNaam().contentEquals(naam)) {
+                match = gebruiker;
+            }
+        }
+        try {
+            if(gebruikerslijst.remove(match)) {
+                removed = true;
+            } else {
+                removed = false;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+            removed = false;
+
+        }
+        return removed;
+    }
+
+    public Integer getAantalStudenten() {
+        return gebruikerslijst.size();
+        // todo dit telt ook admins mee!
+    }
+
 
     @Override
     public String toString() {
-        return "Gebruiker{" +
-                "naam='" + naam + '\'' +
-                ", gebruikerNummer=" + gebruikerNummer +
-                ", resulaten=" + resulaten +
-                ", admin=" + admin +
-                '}';
+        return "Gebruiker" + "\n \t" +
+                "naam='" + naam + "\n \t" +
+                "gebruikerNummer= " + gebruikerNummer + "\n \t" +
+                "admin=" + admin + "\n \n";
     }
 }
