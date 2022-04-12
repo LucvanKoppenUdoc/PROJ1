613609347404b7c3ff30ac6b63f1a6b0d6240015
NielsSchool <101334605+NielsSchool@users.noreply.github.com>
2022-04-12 12:18:59 +0200

    delete gegevenantwoord add unittests

---
+++ Examenpackage/unittests.java
@@ -0,0 +1,23 @@
+import Examenpackage.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+
+    public static void testExamen() {
+
+    }
+
+    public static void testGebruiker() {
+
+    }
+
+    public static void testResultaat() {
+
+    }''
+    public static void testMain() {
+
+    }
+}
