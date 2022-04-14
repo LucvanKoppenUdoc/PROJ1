0931e15cca37f52eb737470a6ef54a95ec61fe3c
BramS21113653 <101334896+BramS21113653@users.noreply.github.com>
2022-04-14 10:41:24 +0200

    test 2

--- Examenpackage/GebruikerTest.java
+++ Examenpackage/GebruikerTest.java
@@ -22,12 +22,23 @@ class GebruikerTest {
     }
 
     @Test
-    void getGebruikersLijst() {
+    void GebruikerslijstTest() {
         //Hier wordt getest of de gebruiker succesvol aan de gebruikerslijst wordt toegevoegd na het aanmaken.
         //Arrange
         Gebruiker gebruiker = new Gebruiker("Bram", 123, "hallo", false);
         //Act
         //Check
         assertTrue(gebruiker.getGebruikersLijst().contains(gebruiker));
+    }
+
+    @Test
+    void GebruikerslijstTest1() {
+        //Hier wordt getest of de grote van gebruikerslijst 1 is na het toevoegen van een student
+        //Arrange
+        Gebruiker gebruiker = new Gebruiker("Bram", 123, "hallo", false);
+        int newSize = gebruiker.getGebruikersLijst().size();
+        //Act
+        //Check
+        assertEquals(newSize, 1);
     }
 }
